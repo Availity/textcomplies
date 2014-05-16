c3e649b6af36c9b49b4926e3ee4de19989dfc272
Rob Warner <rwarner@grailbox.com>
2014-05-16 16:12:41 -0400

    Add tests for lowercase and uppercase letters

--- test/textcomplies.spec.js
+++ test/textcomplies.spec.js
@@ -66,5 +66,115 @@ define(function(require) {
       });
       expect($("#password-compliance").html()).toEqual('<ul><li class="defies">Have at least 1 number</li></ul>');
     });
+
+    it("complies when number of numbers matches (multiple)", function() {
+      var input = $("#my-password");
+      input.val('t31est1');
+      input.textComplies({
+        numNumbers: 3,
+        output: $("#password-compliance"),
+        validateOnStart: true
+      });
+      expect($("#password-compliance").html()).toEqual('<ul><li class="complies">Have at least 3 numbers</li></ul>');
+    });
+
+    it("defies when number of numbers doesn't match (multiple)", function() {
+      var input = $("#my-password");
+      input.val('test12');
+      input.textComplies({
+        numNumbers: 3,
+        output: $("#password-compliance"),
+        validateOnStart: true
+      });
+      expect($("#password-compliance").html()).toEqual('<ul><li class="defies">Have at least 3 numbers</li></ul>');
+    });
+
+    it("complies when number of lowercase letters matches", function() {
+      var input = $("#my-password");
+      input.val('test');
+      input.textComplies({
+        numLowercaseLetters: 1,
+        output: $("#password-compliance"),
+        validateOnStart: true
+      });
+      expect($("#password-compliance").html()).toEqual('<ul><li class="complies">Have at least 1 lowercase letter</li></ul>');
+    });
+
+    it("defies when number of lowercase letters doesn't match", function() {
+      var input = $("#my-password");
+      input.val('TEST');
+      input.textComplies({
+        numLowercaseLetters: 1,
+        output: $("#password-compliance"),
+        validateOnStart: true
+      });
+      expect($("#password-compliance").html()).toEqual('<ul><li class="defies">Have at least 1 lowercase letter</li></ul>');
+    });
+
+    it("complies when number of lowercase letters matches (multiple)", function() {
+      var input = $("#my-password");
+      input.val('test it all');
+      input.textComplies({
+        numLowercaseLetters: 5,
+        output: $("#password-compliance"),
+        validateOnStart: true
+      });
+      expect($("#password-compliance").html()).toEqual('<ul><li class="complies">Have at least 5 lowercase letters</li></ul>');
+    });
+
+    it("defies when number of lowercase letters doesn't match (multiple)", function() {
+      var input = $("#my-password");
+      input.val('TEST IT ALL');
+      input.textComplies({
+        numLowercaseLetters: 5,
+        output: $("#password-compliance"),
+        validateOnStart: true
+      });
+      expect($("#password-compliance").html()).toEqual('<ul><li class="defies">Have at least 5 lowercase letters</li></ul>');
+    });
+
+    it("complies when number of uppercase letters matches", function() {
+      var input = $("#my-password");
+      input.val('tESt');
+      input.textComplies({
+        numUppercaseLetters: 1,
+        output: $("#password-compliance"),
+        validateOnStart: true
+      });
+      expect($("#password-compliance").html()).toEqual('<ul><li class="complies">Have at least 1 uppercase letter</li></ul>');
+    });
+
+    it("defies when number of uppercase letters doesn't match", function() {
+      var input = $("#my-password");
+      input.val('test');
+      input.textComplies({
+        numUppercaseLetters: 1,
+        output: $("#password-compliance"),
+        validateOnStart: true
+      });
+      expect($("#password-compliance").html()).toEqual('<ul><li class="defies">Have at least 1 uppercase letter</li></ul>');
+    });
+
+    it("complies when number of uppercase letters matches (multiple)", function() {
+      var input = $("#my-password");
+      input.val('TEST IT all');
+      input.textComplies({
+        numUppercaseLetters: 5,
+        output: $("#password-compliance"),
+        validateOnStart: true
+      });
+      expect($("#password-compliance").html()).toEqual('<ul><li class="complies">Have at least 5 uppercase letters</li></ul>');
+    });
+
+    it("defies when number of uppercase letters doesn't match (multiple)", function() {
+      var input = $("#my-password");
+      input.val('test iT all');
+      input.textComplies({
+        numUppercaseLetters: 5,
+        output: $("#password-compliance"),
+        validateOnStart: true
+      });
+      expect($("#password-compliance").html()).toEqual('<ul><li class="defies">Have at least 5 uppercase letters</li></ul>');
+    });
   });
 });
