33da79c4b8e97f96b33cfc1b65de3bafc6d14a57
Rob Warner <rwarner@grailbox.com>
2014-05-16 16:13:41 -0400

    Remove unnecessary shim

--- test/test.config.js
+++ test/test.config.js
@@ -6,13 +6,6 @@ var tests = Object.keys(window.__karma__.files).filter(function (file) {
 require.config({
   baseUrl: '/base',
 
-  shim: {
-    'src/jquery.textcomplies': {
-      deps: ['lib/jquery-1.11.1.min'],
-      exports: '$.fn.textComplies'
-    }
-  },
-
   deps: ['lib/jquery-1.11.1.min'].concat(tests),
 
   callback: window.__karma__.start
